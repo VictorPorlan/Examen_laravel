94174075ca300da45ab45b7936bef9b826809a25
VictorPorlan <victorporlaninformatica@gmail.com>
2022-02-10 17:09:55 +0100

    edit

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,36 @@
+<h1>Editar publicacion</h1>
+<form method="post" action="{{ route('publicaciones.update', $post->id) }}">
+    @csrf
+    @method('patch')
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <li> {{ $error }}</li>
+        @endforeach
+    @endif
+    <label>
+        Titulo
+        <input type="text" name="titulo" value="{{ old('titulo', $post->titulo) }}"><br>
+    </label>
+    <label>
+        <input type="checkbox" name="caducable" checked={{ old('caducable', $post->caducable) == true }} /> <label>Caducable</label> <br>
+    </label>
+    <label>
+        <input type="checkbox" name="comentable" checked={{ old('comentable',$post->comentable) == true }} /> <label>Comentable</label> <br>
+    </label>
+    <label>
+        Extracto
+        <input type="text" name="extracto" value="{{ old('extracto',$post->extracto) }}" /><br>
+    </label>
+    <label>
+        Contenido
+        <textarea name="contenido" value="{{ old('contenido', $post->contendio) }}"></textarea><br>
+    </label>
+    <label>
+        Acceso
+    </label>
+    <select name="acceso">
+        <option value="Privado" selected={{ old('acceso') == 'Privado' }}>Privado</option>
+        <option value="Publico" selected={{ old('acceso') == 'Public' }}>Publico</option>
+    </select><br>
+    <button>Editar</button>
+</form>
