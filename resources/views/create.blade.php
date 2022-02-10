8644fd5c3e01ef44b353feecd55747ac4cbdd8ee
VictorPorlan <victorporlaninformatica@gmail.com>
2022-02-10 16:48:06 +0100

    plantillas

---
+++ resources/views/create.blade.php
@@ -0,0 +1,35 @@
+<h1>Crear nueva publicacion</h1>
+<form method="post" action="{{ route('publicaciones.store') }}">
+    @csrf
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <li> {{ $error }}</li>
+        @endforeach
+    @endif
+    <label>
+        Titulo
+        <input type="text" name="titulo" value="{{ old('titulo') }}"><br>
+    </label>
+    <label>
+        <input type="checkbox" name="caducable" checked={{ old('caducable') }} /> <label>Caducable</label> <br>
+    </label>
+    <label>
+        <input type="checkbox" name="comentable" checked={{ old('comentable') }} /> <label>Comentable</label> <br>
+    </label>
+    <label>
+        Extracto
+        <input type="text" name="extracto" value="{{ old('extracto') }}" /><br>
+    </label>
+    <label>
+        Contenido
+        <textarea name="contenido" value="{{ old('contenido') }}"></textarea><br>
+    </label>
+    <label>
+        Acceso
+    </label>
+    <select name="acceso">
+        <option value="Privado" selected={{ old('acceso') == 'Privado' }}>Privado</option>
+        <option value="Publico" selected={{ old('acceso') == 'Public' }}>Publico</option>
+    </select><br>
+    <button>Crear</button>
+</form>
